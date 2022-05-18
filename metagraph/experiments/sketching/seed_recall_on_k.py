45c320f7730ca6a4949ccd581feba1a7b3ea9b80
Alexandru Meterez <ameterez@student.ethz.ch>
2022-05-18 20:52:21 +0200

    Added some bells and whistles to the testing framework

--- metagraph/experiments/sketching/seed_recall_on_k.py
+++ metagraph/experiments/sketching/seed_recall_on_k.py
@@ -4,12 +4,23 @@ import plotly.graph_objects as go
 import time
 import os
 from time import localtime, strftime
+import argparse
+from pprint import pprint
 
 DATASET_DIR = './data'
 METAGRAPH_PATH = "/Users/alex/metagraph/metagraph/build/metagraph"
 MAX_K = 90
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sketch_dim', type=int, default=15)
+    parser.add_argument('--n_times_subsample', type=int, default=5)
+    parser.add_argument('--subsampled_sketch_dim', type=int, default=13)
+    parser.add_argument('--mutation_rate', type=int, default=40)
+    parser.add_argument('--num_query_seqs', type=int, default=1000)
+    parser.add_argument('--parallel', type=int, default=7)
+    args = parser.parse_args()
+
     if not os.path.exists("runs"):
         os.mkdir("runs")
 
@@ -19,12 +30,16 @@ if __name__ == '__main__':
     total_time = 0
 
     config = {
-        'sketch_dim': 15,
-        'n_times_subsample': 2,
-        'subsampled_sketch_dim': 13,
-        'mutation-rate': 40,
-        'num-query-seqs': 1000
+        'sketch_dim': args.sketch_dim,
+        'n_times_subsample': args.n_times_subsample,
+        'subsampled_sketch_dim': args.subsampled_sketch_dim,
+        'mutation-rate': args.mutation_rate,
+        'num-query-seqs': args.num_query_seqs,
+        'parallel': args.parallel
     }
+
+    print("Launching experiment")
+    pprint(config)
 
     for K in K_VALS:
         start = time.time()
@@ -36,6 +51,7 @@ if __name__ == '__main__':
                   f"--subsampled_sketch_dim {config['subsampled_sketch_dim']} " \
                   f"--mutation-rate {config['mutation-rate']} " \
                   f"--num-query-seqs {config['num-query-seqs']} " \
+                  f"--parallel {config['parallel']} " \
                   "-v " \
                   f"-i {DATASET_DIR}/sequence_{K}.dbg " \
                   "--experiment"
@@ -68,5 +84,11 @@ if __name__ == '__main__':
     fig.write_image(os.path.join(experiment_dir, 'fig.png'), scale=1, width=1920, height=1080)
     with open(os.path.join(experiment_dir, 'config.json'), 'w') as outfile:
         json.dump(config, outfile, indent=4)
+    data = {
+        'avg_time': x,
+        'recall': y,
+    }
+    with open(os.path.join(experiment_dir, 'points.json'), 'w') as outfile:
+        json.dump(data, outfile, indent=4)
 
     print(f"Logged experiment at: {experiment_dir}")
