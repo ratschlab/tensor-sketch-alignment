7246c0f4a9d9ee21db331292e91b944cb90e7ed4
Alexandru Meterez <alexandrumeterez@gmail.com>
2022-08-11 22:23:42 +0200

    added all the blunted graphs and vg modifications

--- metagraph/experiments/sketching/generate_dataset.py
+++ metagraph/experiments/sketching/generate_dataset.py
@@ -12,6 +12,10 @@ def get_random_str(main_str, substr_len):
 
 DATASET_DIR = "./data"
 
+get_blunted_path = "/home/alex/benchmark/datagen/GetBlunted/build/get_blunted"
+vg_path = "/home/alex/benchmark/datagen/vg"
+print(vg_path)
+print(get_blunted_path)
 # Clean up
 shutil.rmtree(DATASET_DIR)
 os.mkdir(DATASET_DIR)
@@ -19,7 +23,8 @@ os.mkdir(DATASET_DIR)
 INPUT_SEQ = "sequence.fa"
 ALPHABET = ['A', 'C', 'T', 'G']
 if __name__ == '__main__':
-    
+    assert os.path.exists(vg_path)
+    assert os.path.exists(get_blunted_path)
     parser = argparse.ArgumentParser()
     parser.add_argument("--metagraph-path", type=str, required=True, help="Path to metagraph executable")
     parser.add_argument("--graph-seq-len", type=int, required=True, help="Length of the seq that the graph is generated from")
@@ -45,6 +50,14 @@ if __name__ == '__main__':
 
     # Generate graph
     for K in range(20, MAX_K, 10):
-        build_command = f"{METAGRAPH_PATH} build -k {K} -o {os.path.join(DATASET_DIR, INPUT_SEQ.split('.')[0] + f'_{K}')} {graph_seq_path}"
+        dbg_output = os.path.join(DATASET_DIR, INPUT_SEQ.split('.')[0] + f'_{K}')
+        blunted_dbg_output = os.path.join(DATASET_DIR, INPUT_SEQ.split('.')[0] + f'_{K}_blunted')
+        build_command = f"{METAGRAPH_PATH} build -k {K} -o {dbg_output}.dbg {graph_seq_path}"
+        assemble_command = f"{METAGRAPH_PATH} assemble --to-gfa --compacted --unitigs -o {dbg_output}.gfa {dbg_output}.dbg"
+        blunt_command = f"{get_blunted_path} --input_gfa {dbg_output}.gfa"
+        vg_command = f"{vg_path} -g blunted_{dbg_output}.gfa -v"
         subprocess.run(build_command.split())
+        subprocess.run(assemble_command.split())
+        open(f"{blunted_dbg_output}.gfa", 'w').write(str(subprocess.run(blunt_command.split(), capture_output=True).stdout))
+        open(f"{blunted_dbg_output}.vg", 'w').write(str(subprocess.run(vg_command.split(), capture_output=True).stdout))
         print(f"[LOG] Saved .dbg file from generated sequence - {K}")
--- metagraph/experiments/sketching/run_generate_dataset.sh
+++ metagraph/experiments/sketching/run_generate_dataset.sh
@@ -1,7 +1,8 @@
 ############### Parameters ###############
+rm data/*
 metagraph_path="/home/alex/metagraph/metagraph/build/metagraph"
 max_k=91
-graph_seq_len=10000000
+graph_seq_len=100000
 
 # Generate the dataset
 gen_command="python generate_dataset.py --metagraph-path $metagraph_path --graph-seq-len $graph_seq_len --max-k $max_k"
